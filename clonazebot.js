2b7aab4490f97aabc7f1e2d7c87b7332abee2059
Matias Schwalb <maschwalb@gmail.com>
2019-08-09 17:25:25 -0300

    added some changes

--- clonazebot.js
+++ clonazebot.js
@@ -4,22 +4,52 @@ let alarmTime =  0;
 
 bot.on(['/start', '/hello'], (msg) => msg.reply.text('¡Holis!'));
 bot.on('text', msg => {
-    var id = msg.from.id;
+    const id = msg.from.id;
     var input = msg.text;
     var message = "";
 
-    if (input.startsWith("/set")) {
-        alarmTime = input.slice(5, input.length);
+    if(input.startsWith("/set")) {
+        alarmTime = getParameters(input)
         message = "Seteaste la alarma a las " + alarmTime;
-    } else {
-        message = "No te entiendo";
+    // } else {
+    //     message = "No te entiendo";
+    }
+
+    if(input.startsWith("/alarm")) {
+        message = "Tenes una alarma puesta a las " + alarmTime
+    }
+
+    if(input.startsWith("/time")) {
+        currentTime = getCurrentTime();
+        message = "Son las " + currentTime;
     }
 
     return bot.sendMessage(id, message);
 });
+
+bot.on(alarmTime == getCurrentTime(), (msg) => {
+    const id = msg.from.id;
+    const message = "ALARMA!" + alarmTime;
+    console.log('ALARMA! ' + alarmTime);
+
+    return bot.sendMessage(id, message)
+})
+
    // (msg) => msg.reply.text('Alarma seteada a las ' + alarmTime));
 bot.on(/siempre\sdel\slado\scorrecto/i, (msg) => {
     return msg.reply.photo('https://d1aeri3ty3izns.cloudfront.net/media/3/33238/600/preview_2.jpg');
 });
 
 bot.connect()
+
+function getCurrentTime() {
+    const today = new Date();
+    currentTime = today.getHours() + ":" + today.getMinutes();
+    
+    return currentTime;
+}
+
+function getParameters(input) {
+    atTime = input.slice(5, input.length);
+    return atTime;
+}
